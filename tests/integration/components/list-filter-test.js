f4d441a988ba9e2c3453fa548e14028b66beeab6
BrianArchibald <brianallenarchibald@gmail.com>
2019-03-16 19:47:45 -0500

    finished fileter list and testing

--- tests/integration/components/list-filter-test.js
+++ tests/integration/components/list-filter-test.js
@@ -1,26 +1,74 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, settled, triggerKeyEvent } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { resolve } from 'rsvp';
 
 module('Integration | Component | list-filter', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  hooks.beforeEach(function() {
+    this.actions = {};
+    this.send = (actionName, ...args) => this.actions[actionName].apply(this, args);
+  });
 
-    await render(hbs`{{list-filter}}`);
+  const ITEMS = [{city: 'San Francisco'}, {city: 'Portland'}, {city: 'Seattle'}];
+  const FILTERED_ITEMS = [{city: 'San Francisco'}];
 
-    assert.equal(this.element.textContent.trim(), '');
+  test('should initially load all listings', async function(assert) {
+    assert.expect(2);
+    this.set('filterByCity', () => resolve({ results: ITEMS }));
 
-    // Template block usage:
     await render(hbs`
-      {{#list-filter}}
-        template block text
-      {{/list-filter}}
+      <ListFilter @filter={{action filterByCity}} as |results|>
+        <ul>
+        {{#each results as |item|}}
+          <li class="city">
+            {{item.city}}
+          </li>
+        {{/each}}
+        </ul>
+      </ListFilter>
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    return settled().then(() => {
+      assert.equal(this.element.querySelectorAll('.city').length, 3);
+      assert.dom(this.element.querySelector('.city')).hasText('San Francisco');
+    });
+  });
+
+  test('should update with matching listings', async function(assert) {
+    this.set('filterByCity', (val) => {
+      if (val === '') {
+        return resolve({
+          query: val,
+          results: ITEMS });
+      } else {
+        return resolve({
+          query: val,
+          results: FILTERED_ITEMS });
+      }
+    });
+    await render(hbs`
+      <ListFilter @filter={{action filterByCity}} as |results|>
+        <ul>
+        {{#each results as |item|}}
+          <li class="city">
+            {{item.city}}
+          </li>
+        {{/each}}
+        </ul>
+      </ListFilter>
+    `);
+
+
+    await triggerKeyEvent(this.element.querySelector('.list-filter input'), "keyup", 83);
+
+    return settled().then(() => {
+      assert.ok(this.element.querySelector('.city'), 'one result found');
+      assert.dom(this.element.querySelector('.city')).hasText('San Francisco');
+    });
   });
 });
+
+
